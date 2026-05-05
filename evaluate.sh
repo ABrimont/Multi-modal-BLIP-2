d39388ffbcba27a4840a943a3afd4b110c0f2109
ABrimont <antoine.brimont@telecom-sudparis.eu>
2026-05-05 14:56:40 +0200

    Ajout du nouveau script de traitement

--- evaluate.sh
+++ evaluate.sh
@@ -1,5 +1,5 @@
-# torchrun --standalone --nproc_per_node=2 evaluate.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/eval/caption_vatex_flant5xl_multimodal_eval.yaml
+torchrun --standalone --nproc_per_node=2 evaluate.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/eval/caption_vatex_flant5xl_multimodal_eval.yaml
 
 # torchrun --standalone --nproc_per_node=2 evaluate.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/eval/AudioCaps_flant5xl_multimodal_eval.yaml
 
-torchrun --standalone --nproc_per_node=2 evaluate.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/eval/caption_msrvtt_flant5xl_eval_multimodal.yaml
+# torchrun --standalone --nproc_per_node=2 evaluate.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/eval/caption_msrvtt_flant5xl_eval_multimodal.yaml
