2de7a942ebc09af8043d3116c6f8784954ab9fa3
ABrimont <antoine.brimont@telecom-sudparis.eu>
2026-05-11 15:33:06 +0200

    Change absolute paths to relative paths in train.sh
    Updated file paths in train.sh to use relative paths instead of absolute paths.

--- train.sh
+++ train.sh
@@ -1,26 +1,29 @@
 torchrun --standalone --nproc_per_node=2 \
-  train.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/train/caption_vatex_flant5xl_ft.yaml
+  train.py --cfg-path ./lavis/projects/blip2/train/caption_vatex_flant5xl_ft.yaml
 
 # torchrun --standalone --nproc_per_node=2 \
-#   train.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/train/caption_vatex_flant5xl_multimodal_ft.yaml
+#   train.py --cfg-path ./lavis/projects/blip2/train/caption_vatex_flant5xl_multimodal_ft.yaml
+
+# torchrun --standalone --nproc_per_node=2 \
+#   train.py --cfg-path ./lavis/projects/blip2/train/caption_vatex_flant5xl_separate_ft.yaml
+
+
+
+
+# torchrun --standalone --nproc_per_node=2 \
+#   train.py --cfg-path ./lavis/projects/blip2/train/caption_msrvtt_flant5xl_ft.yaml 
+
+# torchrun --standalone --nproc_per_node=2 \
+#   train.py --cfg-path ./lavis/projects/blip2/train/caption_msrvtt_flant5xl_multimodal_ft.yaml 
+
+# torchrun --standalone --nproc_per_node=2 \
+#   train.py --cfg-path ./lavis/projects/blip2/train/caption_msrvtt_flant5xl_multimodal_scst.yaml
   
-# torchrun --standalone --nproc_per_node=2 \
-#   train.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/train/caption_msrvtt_flant5xl_multimodal_ft.yaml 
 
-# torchrun --standalone --nproc_per_node=2 \
-#   train.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/train/caption_msrvtt_flant5xl_multimodal_scst.yaml
   
-# torchrun --standalone --nproc_per_node=2 \
-#   train.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/train/caption_AudioCaps_flant5xl_multimodal_ft.yaml
 
 # torchrun --standalone --nproc_per_node=2 \
-#   train.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/train/caption_vatex_flant5xl_ft.yaml 
+#   train.py --cfg-path ./lavis/projects/blip2/train/caption_AudioCaps_flant5xl_ft.yaml
 
 # torchrun --standalone --nproc_per_node=2 \
-#   train.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/train/caption_AudioCaps_flant5xl_ft.yaml
-
-# torchrun --standalone --nproc_per_node=2 \
-#   train.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/train/caption_AudioCaps_flant5xl_multimodal_ft.yaml
-
-# torchrun --standalone --nproc_per_node=2 \
-#   train.py --cfg-path /home/abrimont/partage/mllm-video-captioner/lavis/projects/blip2/train/caption_vatex_flant5xl_separate_ft.yaml
+#   train.py --cfg-path ./lavis/projects/blip2/train/caption_AudioCaps_flant5xl_multimodal_ft.yaml
